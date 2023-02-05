32f2675ad7781edcf7dee2c929ae907d6d4bbf73
Pamela Leon <allyleon88@gmail.comgit config --global user.name>
2023-02-05 16:46:21 -0700

    changed dogprofile to dogprofiles in controller event

--- ngDogProfile/src/app/services/dog-profile.service.ts
+++ ngDogProfile/src/app/services/dog-profile.service.ts
@@ -31,4 +31,75 @@ export class DogProfileService {
   }
 
   //TODO: show, create, update, destroy
+
+  create(dog: DogProfile):Observable<DogProfile> {
+    dog.name = '';
+    dog.birthday = '';
+    dog.age = 0;
+    dog.breed = '';
+    dog.weight = 0;
+    dog.chipNumber = 0;
+    dog.registrationNumber = 0;
+    dog.foodBrand = '';
+    dog.amountFood = '';
+    dog.vetHospitalName = '';
+    dog.ownerName = '';
+    return this.http.post<DogProfile>(this.url, dog).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(
+              'DogProfileService.create(): error creating new dog profile: '+ err
+            )
+
+        );
+      })
+    );
+  }
+
+  destroy(id: number):Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(
+              'DogProfileService.destroy(): error deleting dog profile: ' + err
+            )
+        );
+      })
+    );
+  }
+
+  update(dog: DogProfile):Observable<DogProfile> {
+    return this.http.put<DogProfile>(`${this.url}/${dog.id}`, dog).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(
+              'DogProfileService.update(): error updating dog profile:'+ err
+            )
+
+        );
+      })
+    );
+  }
+
+  show(id: number):Observable<DogProfile> {
+    return this.http.get<DogProfile>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(
+              'DogProfileService.show(): error retrieving dog profile:'+ err
+            )
+
+        );
+      })
+    );
+  }
+
 }
